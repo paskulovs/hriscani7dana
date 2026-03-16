6ade9590998bc0df05f89f0229b1dd7d0a6e1e39
Siniša Paskulov <sinisa.paskulov@crealogix.com>
2026-03-16 08:56:23 +0100

    Make cards expandable

--- js/main.js
+++ js/main.js
@@ -31,6 +31,46 @@ jQuery(document).ready(function($) {
 		$('#main-nav').toggleClass('open');
 	});
 
+	$('.about-entry').each(function() {
+		var $entry = $(this);
+		var $toggle = $entry.find('.about-entry__toggle');
+		var $meta = $toggle.find('.about-entry__meta');
+		var $firstParagraph = $entry.find('.about-entry__content p').first();
+
+		if (!$firstParagraph.length || $toggle.find('.about-entry__preview').length) {
+			return;
+		}
+
+		$('<p class="about-entry__preview"></p>')
+			.text($firstParagraph.text())
+			.insertBefore($toggle.find('.about-entry__meta'));
+
+		if ($meta.length && !$entry.find('.about-entry__action').length) {
+			$meta.removeClass('about-entry__meta').addClass('about-entry__action').appendTo($entry);
+		}
+	});
+
+	$('.about-entry__toggle, .about-entry__action').on('click', function() {
+		var $entry = $(this).closest('.about-entry');
+		var $toggle = $entry.find('.about-entry__toggle');
+		var $content = $entry.find('.about-entry__content');
+		var $action = $entry.find('.about-entry__action');
+		var isExpanded = $toggle.attr('aria-expanded') === 'true';
+
+		$toggle.attr('aria-expanded', String(!isExpanded));
+		$entry.toggleClass('is-open', !isExpanded);
+		$action.text(!isExpanded ? 'Prikaži manje' : 'Prikaži više');
+
+		if (isExpanded) {
+			$content.stop(true, true).slideUp(220, function() {
+				$content.attr('hidden', true);
+			});
+			return;
+		}
+
+		$content.removeAttr('hidden').hide().stop(true, true).slideDown(220);
+	});
+
 	function scrollToID(id, speed) {
 		var offSet = 50;
 		var target = $(id);
